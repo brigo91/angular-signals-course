14f48f434c7b8f2fe89b647c65e4c9a1179bd88e
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-10-08 15:20:33 +0200

    4. Writing Your First Angular Signal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -19,5 +19,9 @@ import {toObservable, toSignal, outputToObservable, outputFromObservable} from "
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+    counter = signal(10)
 
+    increment() {
+        this.counter.set(this.counter() + 1)
+    }
 }
