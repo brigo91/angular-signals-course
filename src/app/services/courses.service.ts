ad07d2db992801906e37f5047646d25424446207
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-10-09 11:22:43 +0200

    25. CRUD Service Layer with async / await and the Angular HTTP Client

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -21,4 +21,30 @@ export class CoursesService {
     return response.courses
   }
 
+  async getCourseById(courseId:string): Promise<Course> {
+    const course$ =
+        this.http.get<Course>(
+          `${this.env.apiRoot}/courses/${courseId}`);
+    return firstValueFrom(course$)
+  }
+
+  async createCourse(course: Partial<Course>) : Promise<Course> {
+    const course$ =
+      this.http.post<Course>(`${this.env.apiRoot}/courses`, course)
+    return firstValueFrom(course$);
+  }
+
+  async saveCourse(courseId:string,
+                   changes: Partial<Course>) : Promise<Course> {
+    const course$ =
+      this.http.put<Course>(`${this.env.apiRoot}/courses/${courseId}`,
+        changes)
+    return firstValueFrom(course$);
+  }
+
+  async deleteCourse(courseId:string) {
+    const delete$ =
+      this.http.delete(`${this.env.apiRoot}/courses/${courseId}`);
+    return firstValueFrom(delete$);
+  }
 }
