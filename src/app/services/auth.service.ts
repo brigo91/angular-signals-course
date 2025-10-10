46bec954e3660cabdac045ed8d4958a8f479022d
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-10-10 10:51:01 +0200

    44. Save User Profile In Local Storage - Survive Refreshes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -21,6 +21,25 @@ export class AuthService {
   http = inject(HttpClient);
   router = inject(Router)
 
+  constructor() {
+    this.loadUserFromStorage();
+    effect(() => {
+      const user = this.user();
+      if(user) {
+        localStorage.setItem(USER_STORAGE_KEY,
+          JSON.stringify(user));
+      }
+    });
+  }
+
+  loadUserFromStorage() {
+    const json = localStorage.getItem(USER_STORAGE_KEY);
+    if(json) {
+      const user = JSON.parse(json);
+      this.#userSignal.set(user);
+    }
+  }
+
   async login(email: string, password: string) {
     const login$ = this.http.post<User>(`${environment.apiRoot}/login`, {
       email,
@@ -32,6 +51,7 @@ export class AuthService {
   }
 
   async logout() {
+    localStorage.removeItem(USER_STORAGE_KEY);
     this.#userSignal.set(null);
     await this.router.navigateByUrl('/login');
   }
