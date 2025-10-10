d2996dd9217fff426541f5fc22e43de35a7c01b4
balazs.brigovacz <balazs_brigovacz@epam.com>
2025-10-10 12:40:07 +0200

    47. Model Inputs and Two-Way Data Binding

--- src/app/edit-course-dialog/edit-course-dialog.component.ts
+++ src/app/edit-course-dialog/edit-course-dialog.component.ts
@@ -31,24 +31,26 @@ export class EditCourseDialogComponent {
   form = this.fb.group({
     title: [''],
     longDescription: [''],
-    category: [''],
     iconUrl: ['']
   });
 
   courseService = inject(CoursesService);
 
+  category = signal<CourseCategory>("BEGINNER");
+
   constructor() {
     this.form.patchValue({
       title: this.data?.course?.title,
       longDescription: this.data?.course?.longDescription,
-      category: this.data?.course?.category,
       iconUrl: this.data?.course?.iconUrl
-    })
+    });
+    this.category.set(this.data?.course!.category)
   }
 
   async onSave() {
     const courseProps =
       this.form.value as Partial<Course>;
+    courseProps.category = this.category();
     if (this.data?.mode === "update") {
       await this.saveCourse(this.data?.course!.id, courseProps);
     }
